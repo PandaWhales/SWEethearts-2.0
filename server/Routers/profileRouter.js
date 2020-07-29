3ac96d04da4bba06dd8fe75442f057ffe2ccaece
Steven_Nguyen <stevennnn13@gmail.com>
2020-07-29 13:56:52 -0700

    Finished profile page frontend

--- server/Routers/profileRouter.js
+++ server/Routers/profileRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // get router for explore page
 router.get('/:username', authController.getProfile, (req, res) => {
   // console.log('res.locals.ideas', res.locals.ideas);
+  // console.log(res.locals.userData)
   res.json(res.locals.userData);
 });
 
