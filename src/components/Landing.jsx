a1bc84dea142264d09198aebd8e2a83faac97ef1
Ben Kwak <65254308+bkwak@users.noreply.github.com>
2020-07-30 18:59:11 -0700

    added some animation features

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
 
 /*
  * HELPER FUNCTION: nextPath
@@ -12,29 +13,49 @@ const redirectToPath = (history, path) => {
 };
 
 const Landing = ({ history }) => {
+  //react spring stuff
+  const calc = (x, y) => [
+    -(y - window.innerHeight / 2) / 20,
+    (x - window.innerWidth / 2) / 20,
+    1.1,
+  ];
+  const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+  //
+
   return (
-    <Container fluid className="container" style={{ marginTop: '20vh' }}>
-      <div className="mt-5">
-        <h1 className="d-flex justify-content-center"> Welcome to Scratch Project </h1>
-        <br />
-        <h2 className="mb-5 d-flex justify-content-center">
-          {' '}
-          A place where developers make their dreams come true{' '}
-        </h2>
-        <br />
-      </div>
-      <div className="mt-5 d-flex justify-content-center">
-        <Button
-          className="w-25"
-          onClick={() => redirectToPath(history, '/explore')}
-          size="lg"
-          variant="outline-primary"
-          block
-        >
-          Start Exploring
-        </Button>
-      </div>
-    </Container>
+    //react spring stuff
+    <animated.div
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
+    >
+      <Container fluid className="container" style={{ marginTop: '20vh' }}>
+        <div className="mt-5">
+          <h1 className="d-flex justify-content-center"> Welcome to Scratch Project </h1>
+          <br />
+          <h2 className="mb-5 d-flex justify-content-center">
+            {' '}
+            A place where developers make their dreams come true{' '}
+          </h2>
+          <br />
+        </div>
+        <div className="mt-5 d-flex justify-content-center">
+          <Button
+            className="w-25"
+            onClick={() => redirectToPath(history, '/explore')}
+            size="lg"
+            variant="outline-primary"
+            block
+          >
+            Start Exploring
+          </Button>
+        </div>
+      </Container>
+    </animated.div>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import '../styles/login-signup.scss';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import { Spring, config } from 'react-spring/renderprops';
 
 const Login = (props) => {
   const { authStatus, setAuthStatus } = props;
@@ -49,34 +50,39 @@ const Login = (props) => {
   return loginStatus || authStatus.isLoggedIn ? (
     <Redirect to={{ pathname: '/explore' }} />
   ) : (
-    <div className="login-container">
-      <div className="login-box">
-        <center>
-          <h4>Welcome Back!</h4>
-        </center>
-        <Form>
-          <Form.Group controlId="username">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="username" placeholder="Username" onChange={setInput} />
-          </Form.Group>
+    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} config={config.slow}>
+      {(springProps) => (
+        <div style={springProps} className="login-container">
+          <div className="login-box">
+            <center>
+              <h4>Welcome Back!</h4>
+            </center>
+            <Form>
+              <Form.Group controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Username" onChange={setInput} />
+              </Form.Group>
 
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" onChange={setInput} />
-          </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={setInput} />
+              </Form.Group>
 
-          <Button variant="primary" type="submit" onClick={handleSubmit}>
-            Submit
-          </Button>
-          <div className={loginStatus === false ? 'error-msg' : 'hidden'}>
-            Sorry, your username/password was invalid.
+              <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+              </Button>
+              <div className={loginStatus === false ? 'error-msg' : 'hidden'}>
+                Sorry, your username/password was invalid.
+              </div>
+            </Form>
+            <br></br>
+            <Button variant="primary" type="link" href="/api/auth/github">
+              Log In with GitHub
+            </Button>
           </div>
-        </Form>
-        <Button variant="primary" type="link" href="/api/auth/github">
-          Log In with GitHub
-        </Button>
-      </div>
-    </div>
+        </div>
+      )}
+    </Spring>
   );
 };
 
